e871b2467faf0f48f1f7e726c7506ca140aca31b
Kamil Łętowski <kamilxdr96@gmail.com>
2019-02-02 10:05:18 +0100

    init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SampleComponent from './components/SampleComponent';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SampleComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/SampleComponent.tsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
+export default class SampleComponent extends Component<any, any> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      data: []
+    };
+  }
+
+  componentWillMount = () => {
+    axios.get('./data.json').then(response => {
+      this.setState({
+        data: response.data
+      });
+    });
+  };
+
+  renderData(): any {
+    return this.state.data.map((r: any) => (
+      <li key={r.id}>
+        <h2>{r.title}</h2>
+        <LazyLoadImage effect="blur" src={r.src} alt={r.src} height={'100px'} />
+      </li>
+    ));
+  }
+
+  render() {
+    return (
+      <section>
+        <ul>{this.renderData()}</ul>
+      </section>
+    );
+  }
+}
